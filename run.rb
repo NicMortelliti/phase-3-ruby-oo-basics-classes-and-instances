266117e9fccdb02370219c57a792245c73c3f0c2
NicMortelliti <npmortelliti@gmail.com>
2022-05-26 14:53:59 -0700

    Add commentary

--- run.rb
+++ run.rb
@@ -1,6 +1,9 @@
 class Dog
 end
 
+# Each of the following creates a new
+# instantiation of the Dog class. They
+# each reside in unique areas of memory.
 fido = Dog.new
 snoopy = Dog.new
 lassie = Dog.new
